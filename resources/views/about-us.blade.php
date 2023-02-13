af0d36089c090b033ef66f557c475822ce77d2a1
julsiam <julsestorco031602@gmail.com>
2023-02-13 13:43:17 +0800

    edited landing and about

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
     <h1>About us </h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque incidunt, cum beatae nostrum praesentium similique odio quasi corporis recusandae eligendi, quae laborum in voluptatum eum sint tempore assumenda ducimus asperiores!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque incidunt, cum beatae nostrum praesentium
+        similique odio quasi corporis recusandae eligendi, quae laborum in voluptatum eum sint tempore assumenda ducimus
+        asperiores!</p>
+    <p>edited by Juls</p>
+
 </body>
+
 </html>
